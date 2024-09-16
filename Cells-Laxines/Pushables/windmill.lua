b3f21fc12996c70a31b8d26bf3e78bcd1c714adc
ThePullerCell <97363059+traincrisis@users.noreply.github.com>
2024-09-16 12:48:45 +0000

    Update windmill.lua

--- Cells-Laxines/Pushables/windmill.lua
+++ Cells-Laxines/Pushables/windmill.lua
@@ -1,8 +1,8 @@
-name="Windmill"
-id="CL-WINDMILL"
-author="Agsgt2"
-texture="windmill.png"
-updateIndex=2902
+name = "Windmill"
+id = "CL-WINDMILL"
+author = "Agsgt2"
+texture = "windmill.png"
+updateIndex = 2902
 
 function Push(this, dir, bias)
   return this.Push(dir, this.direction)

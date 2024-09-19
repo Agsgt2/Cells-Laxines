0f7ddd4394f7ba77ffc877f610b7958b2766e009
ThePullerCell <97363059+traincrisis@users.noreply.github.com>
2024-09-19 11:00:40 +0000

    Update fastpusher.lua
    Actual code of Fast Mover from CX-Max

--- Cells-Laxines/Pushers/fastpusher.lua
+++ Cells-Laxines/Pushers/fastpusher.lua
@@ -1,9 +1,22 @@
-name = "Fast Pusher"
-id = "CLS_FASTMOVER"
-author = "Agsgt1 and Agsgt2"
+name = "Cells-Laxines Fast Mover"
+id = "CL_FASTPUSHER"
+author = "traincrisis & agsgt2"
 texture = "fastpusher.png"
+updateIndex = 9000
+
+function Push(this, dir, bias)
+    -- Update bias based on direction
+    if (this.rotation == dir) then
+        bias = bias + 1
+    elseif ((dir + 2) % 4 == this.rotation) then
+        bias = bias - 1
+    end
+    return this.Push(dir, bias)
+end
 
 function Step(this)
-    this.Push(this.direction, 1)
-    this.Push(this.direction, 1)
+    -- Move the Fast Mover 2 cells in the current direction
+    for i = 1, 2 do
+        this.Push(this.direction, 1)
+    end
 end
